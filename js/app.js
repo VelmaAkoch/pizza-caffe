8e23f6dfe0d90fe01f198ef49ebbe529e7cbb17d
VelmaAkoch <velmaakochi1@gmail.com>
2022-03-20 17:01:07 +0300

    app.js

---
+++ js/app.js
@@ -0,0 +1,40 @@
+//pizza object
+class pizza {
+    constructor(size, crust, topping) {
+        this.size = size
+        this.crust = crust
+        this.topping = topping
+    }
+}
+
+//size object
+var size = {
+    s: 600,
+    m: 800,
+    l: 1200 
+}
+
+//crust object
+var crust = {
+    crispy: 300,
+    stuffed: 200,
+    gluten: 100
+}
+
+//cart object
+var cart = []
+
+
+
+//handle form submission
+
+$("document").ready(function() {
+
+    s = $('#pizza-size').val
+    c = $('#pizza-crust').val
+    t = []
+
+    function submitPizza(){
+
+    }
+});
